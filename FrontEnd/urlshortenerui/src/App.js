c81d1293d118323c6665731cf4714ca282b5172a
srichandan <srichandan99@gmail.com>
2023-03-25 11:06:16 +0530

    code re-formatted

--- FrontEnd/urlshortenerui/src/App.js
+++ FrontEnd/urlshortenerui/src/App.js
@@ -1,48 +1,62 @@
 import "./App.css";
 import CopyToClipboard from "react-copy-to-clipboard";
-import {Box, Button, SvgIcon, TextField } from "@mui/material";
+import { Box, Button, SvgIcon, TextField } from "@mui/material";
 import { useState } from "react";
 import axios from "axios";
 function App() {
+  const shortenedUrl = "asdf.com/sas";
 
-  const shortenedUrl="asdf.com/sas";
+  const [url, setUrl] = useState("");
 
-  const [url, setUrl] =useState("");
-
-  const handleUrlChange = (evt)=>{
+  const handleUrlChange = (evt) => {
     setUrl(evt.target.value);
-  }
-  const handleSubmitUrl= ()=>{
+  };
+  const handleSubmitUrl = () => {
     console.log(url);
     // axios.post()
-  }
+  };
 
   return (
     <>
-    <div className="App">
-    
-    <div >
-    <TextField variant="outlined" placeholder="paste the url here:" onChange={
-      handleUrlChange
-    }></TextField>
-      </div>
-  
-      <div >
-        <Button className="textTransformNone" variant="contained"
-          onClick={() => {
-            handleSubmitUrl();
-          }}
-        >
-          Click here to get your URL shortened
-        </Button>
-      </div>
-      
-      <div >
-        <span>
-         <TextField variant="outlined" value={shortenedUrl} readOnly></TextField>
-        <CopyToClipboard text={url}><Button variant="contained" className="textTransforNone">copy url to clipboard</Button></CopyToClipboard>
-        </span>
-      </div>
+      <div className="App">
+        <div>
+          <TextField
+            variant="outlined"
+            placeholder="paste the url here:"
+            onChange={handleUrlChange}
+          ></TextField>
+        </div>
+
+        <div>
+          <Button
+            className="textTransformNone"
+            variant="contained"
+            onClick={() => {
+              handleSubmitUrl();
+            }}
+          >
+            Click here to get your URL shortened
+          </Button>
+        </div>
+
+        <div>
+          <span>
+            <TextField
+              variant="outlined"
+              value={shortenedUrl}
+              readOnly
+            ></TextField>
+            <CopyToClipboard text={url}>
+              <Button
+                style={{ marginTop: "1.5vw" }}
+                variant="contained"
+                className="textTransforNone"
+              >
+                copy url to clipboard
+              </Button>
+            </CopyToClipboard>
+          </span>
+        </div>
       </div>
     </>
   );
