4808e4d59dd1ade2b10c9c4d10e44758cfe7e011
srichandan <srichandan99@gmail.com>
2023-03-25 11:00:50 +0530

    added state to setUrl onchange

--- FrontEnd/urlshortenerui/src/App.js
+++ FrontEnd/urlshortenerui/src/App.js
@@ -1,22 +1,36 @@
 import "./App.css";
 import CopyToClipboard from "react-copy-to-clipboard";
 import {Box, Button, SvgIcon, TextField } from "@mui/material";
+import { useState } from "react";
+import axios from "axios";
 function App() {
 
-  const url="asdf.com/sas";
+  const shortenedUrl="asdf.com/sas";
+
+  const [url, setUrl] =useState("");
+
+  const handleUrlChange = (evt)=>{
+    setUrl(evt.target.value);
+  }
+  const handleSubmitUrl= ()=>{
+    console.log(url);
+    // axios.post()
+  }
 
   return (
     <>
     <div className="App">
     
     <div >
-    <TextField variant="outlined" placeholder="paste the url here:" ></TextField>
+    <TextField variant="outlined" placeholder="paste the url here:" onChange={
+      handleUrlChange
+    }></TextField>
       </div>
   
       <div >
         <Button className="textTransformNone" variant="contained"
           onClick={() => {
-            console.log("button clicked");
+            handleSubmitUrl();
           }}
         >
           Click here to get your URL shortened
@@ -25,7 +39,7 @@ function App() {
       
       <div >
         <span>
-         <TextField variant="outlined" value={url} readOnly></TextField>
+         <TextField variant="outlined" value={shortenedUrl} readOnly></TextField>
         <CopyToClipboard text={url}><Button variant="contained" className="textTransforNone">copy url to clipboard</Button></CopyToClipboard>
         </span>
       </div>
